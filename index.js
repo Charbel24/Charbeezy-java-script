b20b71fcff6ef8942eecbd5af88c0d4bdc3a603a
Charbel <charbelghosn24@hotmail.com>
2024-04-29 20:50:42 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Here i Created variables to get added subtracted multiplied and divided 
+let firstNum = (90 / 10)+1;
+let secondNum = (20+19)+1;
+let thirdNum = (7*6)-3;
+ // Here i created a string to Form a sentence
+let string = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+// Here the string was added to the variable and formed into the alert
+
+alert(string  + `${firstNum} - ${secondNum} - ${thirdNum}`) 
